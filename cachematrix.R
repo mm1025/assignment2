63c2c9c72867b7c6add64cecd883d056b27ee09e
mm1025 <44153444+mm1025@users.noreply.github.com>
2018-11-04 22:44:45 +0800

    Coursera R programming Assignment 2
    Updated on 11/4/18

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## R programming assignment 2
+## Write a funcation to make a matric which is always invertible. 
+## Then write another function to get its inverse
+
+## A function to assign null value in a matric; Then fill in the value by other functions
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+
+}
+
+
+## A function to check if the matrix is not null. If this is a null, returns its inverse
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- inverse(data, ...)
+  x$setinverse(inv)
+  inv
+  
+}
